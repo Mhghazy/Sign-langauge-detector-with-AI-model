fed913efb7f82bc1a6139d9fdfe7602d48d9e4e3
Mhghazy <mahmoudhghazy@hotmail.com>
2024-09-24 20:12:50 +0300

    1st Version of Sign language detector app

---
+++ Sign_language_detector_with_TF/check_cudnn_version.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+print("GPU Available:", tf.config.list_physical_devices('GPU'))
+print("CUDA version:", tf.sysconfig.get_build_info()["cuda_version"])
+print("cuDNN version:", tf.sysconfig.get_build_info()["cudnn_version"])
+
+# Force TensorFlow to use GPU
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)
+else:
+    print("No GPUs found. Make sure TensorFlow can access your GPU.")
+
